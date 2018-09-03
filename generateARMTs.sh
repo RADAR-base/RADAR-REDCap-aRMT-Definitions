df0237b8453a966f3b08b110cb31ca0692f721ca
Amos Folarin (home) <amosfolarin@gmail.com>
2018-09-03 19:02:27 +0100

    pass in different github repos as target

--- generateARMTs.sh
+++ generateARMTs.sh
@@ -1,7 +1,7 @@
-source .env
-#node utilities/index.js $REDCAP_API_URL $DA_REDCAP_API_TOKEN $GITHUB_TOKEN aRMT $DA_LANG
-#node utilities/index.js $REDCAP_API_URL $DE_REDCAP_API_TOKEN $GITHUB_TOKEN aRMT $DE_LANG
-#node utilities/index.js $REDCAP_API_URL $EN_REDCAP_API_TOKEN $GITHUB_TOKEN aRMT $EN_LANG
-#node utilities/index.js $REDCAP_API_URL $ES_REDCAP_API_TOKEN $GITHUB_TOKEN aRMT $ES_LANG
-node utilities/index.js $REDCAP_API_URL $IT_REDCAP_API_TOKEN $GITHUB_TOKEN aRMT $IT_LANG
-#node utilities/index.js $REDCAP_API_URL $NL_REDCAP_API_TOKEN $GITHUB_TOKEN aRMT $NL_LANG
+source .env                                                                                                                                                                                                                                                                                                                                                                                                                       
+node utilities/index.js $REDCAP_API_URL $DA_REDCAP_API_TOKEN $GITHUB_TOKEN aRMT $DA_LANG $GITHUB_REPO_OWNER $GITHUB_REPO_NAME $GITHUB_REPO_DIR
+node utilities/index.js $REDCAP_API_URL $DE_REDCAP_API_TOKEN $GITHUB_TOKEN aRMT $DE_LANG $GITHUB_REPO_OWNER $GITHUB_REPO_NAME $GITHUB_REPO_DIR
+node utilities/index.js $REDCAP_API_URL $EN_REDCAP_API_TOKEN $GITHUB_TOKEN aRMT '' $GITHUB_REPO_OWNER $GITHUB_REPO_NAME $GITHUB_REPO_DIR       # only seems to work with a literal for EN_LANG
+node utilities/index.js $REDCAP_API_URL $ES_REDCAP_API_TOKEN $GITHUB_TOKEN aRMT $ES_LANG $GITHUB_REPO_OWNER $GITHUB_REPO_NAME $GITHUB_REPO_DIR 
+node utilities/index.js $REDCAP_API_URL $IT_REDCAP_API_TOKEN $GITHUB_TOKEN aRMT $IT_LANG $GITHUB_REPO_OWNER $GITHUB_REPO_NAME $GITHUB_REPO_DIR 
+node utilities/index.js $REDCAP_API_URL $NL_REDCAP_API_TOKEN $GITHUB_TOKEN aRMT $NL_LANG $GITHUB_REPO_OWNER $GITHUB_REPO_NAME $GITHUB_REPO_DIR
